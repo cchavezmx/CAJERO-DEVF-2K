bb22a7e1aa9d89a252dfea238124c0d3ed93eb4b
cchavezmx99 <carlos.chavez@99minutos.com>
2023-06-28 20:11:28 -0600

    Se añade metodo de clasList y appendChild

--- cajero.js
+++ cajero.js
@@ -2,6 +2,9 @@
 
 const form = document.querySelector("#cajero")
 const retiroTag = document.querySelector("#retiro")
+const notification = document.querySelector("#notification")
+const closeNotification = document.querySelector("#close_notification")
+const retiroButton = document.querySelector("#retiro-button")
 
 // variables globables
 let cantidadRetirada = 0
@@ -18,6 +21,7 @@ form.addEventListener('submit', function(e){
   e.preventDefault()  
   // FormData
   const formData = new FormData(e.target)
+  // este debe ser de la propiedad nombre del input
   const retiro = formData.get("cantidadRetiro")
 
   cantidadRetirada = cantidadRetirada + Number(retiro)
@@ -25,4 +29,46 @@ form.addEventListener('submit', function(e){
   localStorage.setItem('cantidadRetirada', JSON.stringify(cantidadRetirada))
   // renderizas o dibujar
   retiroTag.textContent = cantidadRetirada
+  
+  form.reset()
+})
+
+
+// PRINCIPIOS SOLID //
+retiroButton.addEventListener('click', () => {
+  const formData = new FormData(form)
+  // este debe ser de la propiedad nombre del input
+  const retiro = formData.get("cantidadRetiro")
+
+  if (Number(retiro) > cantidadRetirada) {    
+    notification.classList.toggle("hidden")
+    const span = document.createElement("span")
+    // span.textContent = "Fondos insuficientes"
+    span.innerHTML = "<h1 class='h1-font'>Fondos <strong>insuficientes</strong></h1>"
+    notification.appendChild(span)
+    return
+  }
+
+  cantidadRetirada = cantidadRetirada - Number(retiro)
+  // guarde la cantidad en mi BD
+  localStorage.setItem('cantidadRetirada', JSON.stringify(cantidadRetirada))
+  // renderizas o dibujar
+  retiroTag.textContent = cantidadRetirada
+  form.reset()
+})
+
+closeNotification.addEventListener('click', function(){
+  // notification.classList.toggle("hidden")  
+  const listaClases = notification.classList
+
+  for (clase of listaClases){
+    console.log(clase)
+    if (clase === 'hidden'){      
+      notification.classList.remove('hidden')
+      return 
+    } else {      
+      notification.classList.add('hidden')
+      return
+    }
+  }
 })
